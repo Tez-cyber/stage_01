214ef2f63d07437ad5d794d46caf18109429254d
Salami Azeez <salamitopeazeez@gmail.com>
2024-07-01 16:55:13 +0100

    get user ip address using ip package

---
+++ index.js
@@ -0,0 +1,20 @@
+import http from 'http'
+import ip from 'ip'
+
+
+
+const PORT = 8800
+const server = http.createServer((req, res) => {
+    const clientIp = ip.address();
+    const responseObj = {
+        client_ip: clientIp
+    };
+
+    res.writeHead(200, { 'Content-Type': 'application/json' });
+    res.end(JSON.stringify(responseObj));
+
+})
+
+server.listen(PORT, () => {
+    console.log(`Server listening on port: ${PORT}`)
+})
